c015e1257e6f8f8ba03dc3e4d408c9aa51ab55b2
battheresa <chawisa.batt@gmail.com>
2021-01-25 13:35:01 +0700

    stripe payment system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import react from 'react';
+import React, { useState, useEffect } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+import axios from './axios';
+import Checkout from './components/Checkout';
+
 import './App.css';
 
+
 function App() {
+    const [ promise, setPromise ] = useState('');
+
+    useEffect(() => {
+        const getPromise = async () => {
+            await axios.get('/key').then((response) => {
+                setPromise(loadStripe(response.data));
+            });
+        };
+
+        getPromise();
+    }, []);
+
     return (
         <div className='app'>
-            
+            {promise && <Elements stripe={promise}>
+                <Checkout />
+            </Elements>}
         </div>
     );
 }
---
+++ src/components/Checkout.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+import axios from '../axios.js';
+
+
+function Checkout() {
+    const [ input, setInput ] = useState(0);
+    const [ secret, setSecret ] = useState(true);
+
+    const [ error, setError ] = useState(null);
+    const [ disabled, setDisabled ] = useState(true);
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const getSecret = async () => {
+        const response = await axios({
+            method: 'POST',
+            url: `/purchase/order?total=${Math.round(input * 100)}`
+        });
+
+        setSecret(response.data.secret);
+    };
+
+    const handlePayment = async (e) => {
+        e.preventDefault();
+        
+        await getSecret();
+
+        const payload = await stripe.confirmCardPayment(secret, { 
+            payment_method: {
+                card: elements.getElement(CardElement)
+            }
+        });
+
+        console.log(payload);
+    }
+
+    const handleChange = (e) => {
+        setError(e.error ? e.error.message : '');
+        setDisabled(e.empty);
+    };
+
+    return (
+        <div className='checkout'>
+            <CardElement onChange={handleChange} />
+            <input value={input} onChange={(e) => setInput(e.target.value)} type='number'/>
+            <button onClick={handlePayment} disabled={disabled || error}>purchase</button>
+        </div>
+    );
+}
+
+export default Checkout;
